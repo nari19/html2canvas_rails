e2aee81fbb8d05f7a87bbfe649344ed0f1cd2f10
nari19 <kazuki7793996@gmail.com>
2019-02-19 09:34:08 +0900

    test8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # トップ画面に設定したビューを表示させる
+  root 'tasks#index'
+
+  # RESTfulなリソースにするために必要なマッピングを定義
   resources :tasks
 end
