119a59c5109ff587a749dfbbc2e478c9740ea192
nari19 <kazuki7793996@gmail.com>
2019-02-19 19:18:38 +0900

    Update gemfile for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # トップ画面に設定したビューを表示させる
-  root 'tasks#index'
-
   # RESTfulなリソースにするために必要なマッピングを定義
   resources :tasks
+  # トップ画面に設定したビューを表示させる
+  root 'tasks#index'
 end
